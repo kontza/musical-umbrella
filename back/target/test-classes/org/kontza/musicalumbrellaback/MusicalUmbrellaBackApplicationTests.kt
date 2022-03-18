1d693623896a602b5035b155502b69d518d1db8e
Rumpu-Jussi <kontza@gmail.com>
2022-03-18 21:51:52 +0200

    Initial commit.

---
+++ back/target/test-classes/org/kontza/musicalumbrellaback/MusicalUmbrellaBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kontza.musicalumbrellaback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusicalUmbrellaBackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
