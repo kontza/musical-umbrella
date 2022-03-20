5f0e3e334ab519e535125a9c2f27e0a4ae7e1c6b
Rumpu-Jussi <kontza@gmail.com>
2022-03-20 20:25:41 +0200

    Recreated back.

--- back/src/main/kotlin/org/kontza/musicalumbrellaback/MusicalUmbrellaBackApplication.kt
+++ back/src/main/kotlin/org/kontza/musicalumbrellaback/MusicalUmbrellaBackApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MusicalUmbrellaApplication
+class MusicalUmbrellaBackApplication
 
 fun main(args: Array<String>) {
-	runApplication<MusicalUmbrellaApplication>(*args)
+	runApplication<MusicalUmbrellaBackApplication>(*args)
 }
